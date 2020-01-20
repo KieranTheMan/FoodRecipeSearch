d529f13b5c52947139d8a13eb7412dbb5e499471
KieranTheMan <kieran9fowler@gmail.com>
2020-01-20 17:40:33 +0000

    initialise async await and include fetch api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Form from './components/Form';
 
 const API_KEY = '01f595e31a6643bc981009465687ee75'
- 
+//asyn await then fetch api data 
 class App extends Component {
-  getRecipe = (e) => {
+  getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value
     e.preventDefault(); // prefevents the fullpage from refreshing
-    const api_call = 'https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=API-KEY'
+    const api_call = await fetch(`https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${API_KEY}`)
     console.log(recipeName);
   }
 
