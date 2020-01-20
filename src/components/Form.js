7a31b18c87bfc83a0239824dc8f14fcf4e2b7d9b
KieranTheMan <kieran9fowler@gmail.com>
2020-01-20 14:58:54 +0000

    create stateless form and export

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//stateless component
+
+const Form = () => (
+    <form>
+        <input type='text'/>
+        <button>Search</button>
+    </form>
+);
+
+export default Form;
