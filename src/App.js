8b78defd99005680daf72b6e3f108e95d184e069
KieranTheMan <kieran9fowler@gmail.com>
2020-01-20 16:16:41 +0000

    change quots to single quots

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
 
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 clsssName='App-title'>Recipe Search</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
       </div>
