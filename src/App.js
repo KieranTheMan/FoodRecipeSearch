9f2951de743cffac17c0be886d148a51feda2d82
KieranTheMan <kieran9fowler@gmail.com>
2020-01-21 15:03:56 +0000

    create state add foods, and setstate for foods, create map function for food titles

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Form from './components/Form';
 const API_KEY = '040fb28bea6246d79d1a029c1f6f212e'
 //asyn await then fetch api data 
 class App extends Component {
+  state = {
+    foods: []
+  }
   getFood = async (e) => {
     const foodName = e.target.elements.foodName.value
     e.preventDefault(); // prefevents the fullpage from refreshing
     const api_call = await fetch(`https://api.spoonacular.com/food/products/search?query=${foodName}&apiKey=${API_KEY}`)
     const data = await api_call.json();
-    console.log(data);
+    this.setState({foods: data.products})
+    console.log(this.state.foods);
   }
 
   render () {
@@ -20,6 +24,9 @@ class App extends Component {
           <h1 className='App-title'>Food Search</h1>
         </header>
         <Form getFood={this.getFood}/>
+        {this.state.foods.map((food) => {
+            return <p key={food.id}>{food.title}</p>
+          })}
       </div>
     );
   }
