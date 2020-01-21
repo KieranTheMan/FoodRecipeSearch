359fb4bf2177dafcd6b77e63c1e1ac88172d037b
KieranTheMan <kieran9fowler@gmail.com>
2020-01-21 17:10:42 +0000

    add classNames, input=form_input, button=form_button

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,9 @@ import React from 'react'
 //stateless component
 //name atribute reads values from inout
 const Form = props => (
-    <form onSubmit= {props.getFood}>
-        <input type='text' name='foodName'/>
-        <button>Search</button>
+    <form onSubmit= {props.getFood} style={{ marginBottom:'2rem'}}>
+        <input className='form__input'type='text' name='foodName'/>
+        <button className='form__button'>Search</button>
     </form>
 );
 
