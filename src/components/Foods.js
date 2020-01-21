5e112e777da3f2e3520bd4161297f4fe4617e098
KieranTheMan <kieran9fowler@gmail.com>
2020-01-21 17:06:41 +0000

    add className to divs, container,row,col-md-4,food__box-img, food__text

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const Foods = props => (
-    <>
-        {props.foods.map((food) => {
-            return (
-              <>
-                <img key={food.id}src={food.image} alt={food.title}/>
-                <p>{food.title}</p>
-              </>
-            ) 
-          })}
-    </>
+    <div className='container'>
+        <div className='row'>
+            {props.foods.map((food) => {
+                return (
+                    <div className='col-md-4'>
+                        <div className='food__box'>
+                            <img 
+                                className='food__box-img'
+                                src={food.image} 
+                                alt={food.title}
+                            />
+                            <div className='food__text'>
+                                <h5>{food.title}</h5>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    </div>
 );
 
 export default Foods;
