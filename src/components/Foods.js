4cd8174994f314790fa311401da7d93873cee00f
KieranTheMan <kieran9fowler@gmail.com>
2020-01-21 18:11:32 +0000

    add style marginBottom, classNames foods_title, food__text and key=id

--- src/components/Foods.js
+++ src/components/Foods.js
@@ -5,15 +5,15 @@ const Foods = props => (
         <div className='row'>
             {props.foods.map((food) => {
                 return (
-                    <div className='col-md-4'>
+                    <div key={food.id} className='col-md-4' style={{ marginBottom: '5rem'}}>
                         <div className='food__box'>
                             <img 
-                                className='food__box-img'
+                                className='food__box'
                                 src={food.image} 
                                 alt={food.title}
                             />
                             <div className='food__text'>
-                                <h5>{food.title}</h5>
+                                <h1 className='foods__title'>{food.title}</h1>
                             </div>
                         </div>
                     </div>
