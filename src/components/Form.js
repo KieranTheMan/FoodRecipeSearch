012433e3cf87e43e3ea39e1935a557d950cebdbb
KieranTheMan <kieran9fowler@gmail.com>
2020-01-22 12:58:57 +0000

    add required in to the input tag

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import React from 'react'
 //name atribute reads values from inout
 const Form = props => (
     <form onSubmit= {props.getFood} style={{ marginBottom:'2rem'}}>
-        <input className='form__input'type='text' name='foodName'/>
+        <input className='form__input' required type='text' name='foodName'/>
         <button className='form__button'>Search</button>
     </form>
 );
