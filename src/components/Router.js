50a9d6e433b3a8facb4253fbf0f93b53d158d550
KieranTheMan <kieran9fowler@gmail.com>
2020-01-23 17:04:26 +0000

    add Switch to BrowserRouter and export Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,11 @@ import Food from './Food';
 
 const Router = () => (
     <BrowserRouter>
-        <Route path = '/' component={App} />
-        <Route path = '/food' component={Food}/>
+        <Switch>
+            <Route path='/' component={App} exact/>
+            <Route path='/food/:id' component={Food}/>
+        </Switch>
     </BrowserRouter>
-)
+);
+
+export default Router;
