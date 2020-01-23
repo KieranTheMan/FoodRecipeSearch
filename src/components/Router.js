45ebeea178cd3986779c7614b7e61d2efe3dd034
KieranTheMan <kieran9fowler@gmail.com>
2020-01-23 16:26:56 +0000

    add route for food component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../App';
-
+import Food from './Food';
 
 const Router = () => (
     <BrowserRouter>
         <Route path = '/' component={App} />
+        <Route path = '/food' component={Food}/>
     </BrowserRouter>
 )
