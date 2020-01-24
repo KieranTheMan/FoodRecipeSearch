f60be77a59ab89edb91a226332d488bedb4233a4
KieranTheMan <kieran9fowler@gmail.com>
2020-01-24 19:55:27 +0000

    create state, add async compnentDidMount include const title, req and res

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+const API_KEY = '040fb28bea6246d79d1a029c1f6f212e'
 
 class Food extends Component {
+
+    state = {
+        activefood: []
+    }
+
+    componentDidMount = async () => {
+            const title = this.props.location.state.food
+            const req = await fetch(`https://api.spoonacular.com/food/products/search?query=${title}&apiKey=${API_KEY}`)
+            const res = await req.json();
+            console.log(res.products[0]);
+        }
+
     render() {
         console.log(this.props);
         return (
