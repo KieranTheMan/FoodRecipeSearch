6735e3766ff4bf41b32380f26233dfc583423ef0
KieranTheMan <kieran9fowler@gmail.com>
2020-01-25 19:06:51 +0000

    create active food state as const, add classNames container, active-food, acitive-food_img and active food_title

--- src/components/Food.js
+++ src/components/Food.js
@@ -4,20 +4,27 @@ const API_KEY = '040fb28bea6246d79d1a029c1f6f212e'
 class Food extends Component {
 
     state = {
-        activefood: []
+        activeFood: []
     }
 
     componentDidMount = async () => {
             const title = this.props.location.state.food;
             const req = await fetch(`https://api.spoonacular.com/food/products/search?query=${title}&apiKey=${API_KEY}`);
             const res = await req.json();
-            this.setState({activefood: res.products[0]});
-            console.log(this.state.activefood);
+            this.setState({activeFood: res.products[0]});
+            console.log(this.state.activeFood);
         }
 
     render() {
+        const food = this.state.activeFood;
         return (
-            <div>Food Component</div>
+            <div className='container'>
+                <div className='active-food'>
+                    <img className='active-food_img' src={food.image} alt =
+                    {food.title}/>
+                    <h3 className='acitve-food_title'>{food.title}</h3>
+                </div>
+            </div>
         );
     }
 }
